1dfd1e9a3b602210234a85e80b9b3ce4885b36ca
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-03 00:53:29 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,12 @@
+import sys
+
+n=int(sys.stdin.readline())
+
+for i in range(1,n+1):
+    for j in range(n-i-1,-1,-1):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
