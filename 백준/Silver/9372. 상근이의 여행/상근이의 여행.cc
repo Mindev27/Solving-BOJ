c9f2f35f3cf87efc2300cb15850e989cca845da0
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-03 02:54:05 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int testCase, n, m;
+    
+  cin >> testCase;
+
+  while(testCase--){
+    cin >> n >> m;
+
+    while(m--){
+      int x, y;
+      cin >> x >> y;
+
+    }
+    cout << n - 1 << "\n";
+
+  } 
+}
