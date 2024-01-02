04db8d986eb20765a81f78cb34205dac88ef2dea
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-03 01:18:53 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main(){
+
+  printf("         ,r\'\"7\n");
+
+  printf("r\`-_   ,\'  ,/\n");
+
+  printf(" \\. \". L_r\'\n");
+
+  printf("   \`~\\/\n");
+
+  printf("      |\n");
+
+  printf("      |");
+
+}
