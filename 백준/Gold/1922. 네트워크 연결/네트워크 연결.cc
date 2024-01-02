6a29bd4d884c8d0a473f4caeb653c6e1f5438ccd
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-03 03:19:36 +0900

    [Gold IV] Title: 네트워크 연결, Time: 104 ms, Memory: 10380 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+const int MAX = 100001;
+vector<pair<int, int>> linked[MAX];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> dist;
+bool visited[MAX] = {};
+
+int prim(int n);
+
+int main() {
+  int v, e;
+  cin >> v >> e;
+
+  // 간선 정보 저장
+  for(int i = 0; i < e; i++) {
+    int start, end, weight;
+    cin >> start >> end >> weight;
+
+    linked[start].push_back({weight, end});
+    linked[end].push_back({weight, start});
+  }
+
+  cout << prim(v);
+}
+
+// 프림 알고리즘
+int prim(int n) {
+  int result = 0;
+
+  //초기화
+  for(int i = 1; i <= n; i++)
+    visited[i] = false;
+
+
+  dist.push({0, 1}); // 1로 시작
+
+  for(int i = 1; i <= n; i++){
+    int cur, minDist = INT_MAX;
+
+    while(!dist.empty()){
+      cur = dist.top().second;
+      if(!visited[cur]){
+        minDist = dist.top().first;
+        break;
+      }
+      dist.pop();
+    }
+
+    result += minDist; // 전체값에 더해주기
+    visited[cur] = true; // 방문표시
+    for(auto next: linked[cur]){ // 연결된 정점의 다음 정점들과 가중치 정보 전달
+      dist.push(next);
+    }
+  }
+
+  return result;
+}
