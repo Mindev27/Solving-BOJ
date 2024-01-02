57e20cd83480a07c4bf98599f357a6259eb85f83
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-03 04:13:24 +0900

    [Platinum IV] Title: 안수빈수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/16680. 안수빈수/안수빈수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#define ll long long
+using namespace std;
+
+int main() {
+  // (void)freopen("input.txt", "r", stdin);
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int T; cin >> T;
+  while(T--){
+    ll N; cin >> N;
+    cout << N * (ll)999999999 << '\n';
+  }
+  return 0;
+}
