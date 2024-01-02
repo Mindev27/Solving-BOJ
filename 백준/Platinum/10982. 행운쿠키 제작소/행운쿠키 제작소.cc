1d6a9849f6171267976c5529b5d870bb870165a2
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-03 03:08:20 +0900

    [Platinum IV] Title: 행운쿠키 제작소, Time: 1212 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Platinum/10982. 행운쿠키 제작소/행운쿠키 제작소.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  const int INF = 999999;
+  vector<pair<int, int>> v;
+  int testCase, dp[100001] = {};
+
+  
+
+  cin >> testCase;
+
+  while(testCase--){
+    int n, result;
+    cin >> n;
+    
+    //dp배열 초기화
+    dp[0] = 0;
+    for(int i = 1; i <= 100000; i++){
+      dp[i] = INF;
+    }
+  
+    int sumB = 0; // B누적값
+    
+    for(int i = 0; i < n; i++){
+      int a, b;
+      cin >> a >> b;
+      sumB += b;
+  
+      // dp배열 업데이트
+      // 여러번 계산되는 것을 피하기위해 역순으로 계산
+      for(int j = 100000; j >= 0; j--){
+        if(j - b >= 0){          // 범위 내의 값이며
+          if(dp[j - b] != INF){ // 이전 값이 존재한다면
+            dp[j] = min(dp[j - b] + a, dp[j]);
+          }
+        }
+      }
+  
+      //순회하면서 최솟값 탐색
+      result = INF;
+      for(int i = 0; i <= 100000; i++){
+        if(dp[i] != INF)
+          result = min(result, max(sumB - i, dp[i]));
+      }
+    }
+  
+    cout << result << "\n";
+  }  
+}
