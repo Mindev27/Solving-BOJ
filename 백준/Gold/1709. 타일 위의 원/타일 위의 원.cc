990eddaaafee33fa33486b9db2a4c18f2a516a5c
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-01-22 16:30:14 +0900

    [Gold V] Title: 타일 위의 원, Time: 260 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1709. 타일 위의 원/타일 위의 원.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int, int>
+
+using namespace std;
+
+ll N, R;
+
+void solve() {
+  R = N / 2; // 반지름 길이
+  ll rSqare = R * R;
+
+  ll cnt = 0;
+  for(ll x = 0, y = R - 1; x <= R && y >= 0;) {
+    ll dist = (x + 1) * (x + 1) + y * y;
+
+    if(dist < rSqare) { // 포함되는 경우
+      x++; cnt++;
+    }
+    else if(dist == rSqare) { // 정확히 점을 지나는 경우
+      x++; y--; cnt++;
+    }
+    else{       // 포함되지 않는 경우
+      y--; cnt++;
+    }
+  }
+  cout << 4 * cnt;
+}
+
+int main(void) {
+  // (void)freopen("input.txt", "r", stdin);
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> N;
+  solve();
+
+  return 0;
+}
