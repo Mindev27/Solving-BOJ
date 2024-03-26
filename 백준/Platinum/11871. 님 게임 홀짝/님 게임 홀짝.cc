66f37d276bf7da084bbe485f905c736d62fca0d0
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-03-26 15:39:10 +0900

    [Platinum IV] Title: 님 게임 홀짝, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/11871. 님 게임 홀짝/님 게임 홀짝.cc
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <stack>
+
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+using namespace std;
+
+ll N;
+vector<ll> p;
+
+void input() {
+  cin >> N;
+  for(int i = 0; i < N; i++) {
+    ll x; cin >> x;
+    p.push_back(x);
+  }
+}
+
+void solve() {
+  ll grundy = 0;
+  for(ll &i : p) {
+    if(i % 2 == 0) 
+      grundy = grundy ^ (i / 2 - 1);
+    else
+      grundy = grundy ^ (i / 2 + 1);
+  }
+
+  if(grundy) cout << "koosaga";
+  else cout << "cubelover";
+}
+
+int main() {
+  // (void)freopen("input.txt", "r", stdin);
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  input();
+  solve();
+
+  return 0;
+}
