3cd0bf72fe04ca5c82c58c48e5893924412cb32f
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-06-14 17:00:46 +0900

    [Bronze III] Title: Square Pasture, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14173. Square Pasture/Square Pasture.py
@@ -0,0 +1,7 @@
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+min_x = min(x1, x3)
+max_x = max(x2, x4)
+min_y = min(y1, y3)
+max_y = max(y2, y4)
+print(max(max_x - min_x, max_y - min_y) ** 2)
