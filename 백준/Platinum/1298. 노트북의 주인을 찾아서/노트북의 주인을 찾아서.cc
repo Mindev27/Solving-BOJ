d4e003b3e8fc631c83cc26dd71218ca35fb8785e
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-09-03 18:52:16 +0900

    [Platinum IV] Title: 노트북의 주인을 찾아서, Time: 0 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.cc
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+const int INF = 0x3f3f3f3f;
+const int MAX = 205;
+
+// 간선 구조체
+struct Edge {
+  int to, c, f;
+  Edge *dual;
+  Edge() : Edge(-1, 0) {}
+  Edge(int to1, int c1) : to(to1), c(c1), f(0), dual(nullptr) {}
+  int spare() { return c - f; }
+  void addFlow(int f1) {
+    f += f1;
+    dual->f -= f1;
+  }
+};
+
+int N, M, S, T;
+vector<Edge*> adj[MAX];
+
+// 간선 만드는 함수
+void makeEdge(int from, int to, int w) {
+  Edge *e1 = new Edge(to, w), *e2 = new Edge(from, 0);
+  e1->dual = e2;
+  e2->dual = e1;
+  adj[from].push_back(e1);
+  adj[to].push_back(e2);
+}
+
+
+void solve() {
+  cin >> N >> M;
+  S = 0, T = MAX - 1;
+  // 사람:1~N, 노트북:N+1~2N 
+  for(int i = 0; i < M; i++) {
+    int x, y; cin >> x >> y;
+    makeEdge(x, N+y, 1);
+  }
+  for(int i = 1; i <= N; i++) {
+    makeEdge(S, i, 1);
+    makeEdge(N+i, T, 1);
+  }
+
+  // Edmond-Karp 알고리즘
+  int ans = 0;
+  int prev[MAX];
+  Edge *path[MAX];
+
+  while(true) {
+    fill(prev, prev + MAX, -1);
+    queue<int> q;
+    q.push(S); // 싱크에서 시작
+
+    while(!q.empty() && prev[T] == -1) {
+      int cur = q.front();
+      q.pop();
+      for(Edge *e : adj[cur]) {
+        int next = e->to;
+
+        // 용량이 남아있고, 방문하지 않았으면
+        if(e->spare() > 0 && prev[next] == -1) {
+          q.push(next);
+          prev[next] = cur;
+          path[next] = e;
+          if(next == T) break; // 싱크 도착
+        }
+      }
+    }
+
+    // 싱크에 도달하지 못했으면 끝
+    if(prev[T] == -1) break;
+
+    // 최대유량을 찾고 각 간선에 흘러보내기
+    int flow = INF;
+    for(int i = T; i != S; i = prev[i]) {
+      flow = min(flow, path[i]->spare());
+    }
+    for(int i = T; i != S; i = prev[i]) {
+      path[i]->addFlow(flow);
+    }
+    ans += flow;
+  }
+
+  cout << ans;
+}
+
+int main() {
+  // (void)freopen("input.txt", "r", stdin);
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+
+  return 0;
+}
