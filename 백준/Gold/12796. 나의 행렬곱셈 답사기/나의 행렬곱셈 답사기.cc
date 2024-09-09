f377433fc35c63f0e6449c08505edd7dc8c1c0b9
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-09-09 15:15:06 +0900

    [Gold V] Title: 나의 행렬곱셈 답사기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12796. 나의 행렬곱셈 답사기/나의 행렬곱셈 답사기.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve() {
+  int K;
+  cin >> K;
+
+  cout << "3\n";
+  cout << "1 1 1 " << K + 1;
+}
+
+int main() {
+  // (void)freopen("input.txt", "r", stdin);
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+
+  return 0;
+}
+
