d2326459edbfc67e63fc95fd815eb6b7717ade42
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-09-27 18:44:57 +0900

    [Gold IV] Title: 데이터 만들기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/7140. 데이터 만들기 1/데이터 만들기 1.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int, int>
+
+void solve() {
+  cout << "101\n";
+  cout << "1 1 1\n";
+  for(int i = 0; i < 100; i++) {
+    cout << "0\n";
+  }
+  cout << "1\n";
+  cout << "1 2\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+
+  return 0;
+}
