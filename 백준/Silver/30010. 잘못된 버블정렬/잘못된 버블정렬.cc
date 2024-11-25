b95e3956b7bcf24e67efc4742912e423a331d40a
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-11-25 21:28:48 +0900

    [Silver V] Title: 잘못된 버블정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve() {
+    int N; cin >> N;
+    for(int i = N; i >= 1 ;i--) {
+        cout << i << ' ';
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+
+    return 0;
+}
