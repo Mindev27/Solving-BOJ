73698b52e9e748ac2a719db60f8817cfa4cbd0a6
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-12-19 20:49:28 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 180 ms, Memory: 72332 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -1,110 +1,97 @@
-#include <algorithm>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 #define ll long long
-#define ld long double
 #define X first
 #define Y second
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 
-using namespace std;
+int N;
+ll arr[1000005];
+ll seg[4000050];
+ll lazy_arr[4000050];
 
-int n, m, k;
-vector<ll> arr, tree, lazy;
+ll build(int node, int start, int end) {
+    if (start == end) {
+        seg[node] = arr[start];
+    }
+    else {
+        int mid = (start + end) / 2;
+        build(2 * node, start, mid);
+        build(2 * node + 1, mid + 1, end);
+        seg[node] = seg[2 * node] + seg[2 * node + 1];
+    }
+    return seg[node];
+}
 
-void build(int node, int start, int end) {
-  if (start == end) {
-    tree[node] = arr[start];
-  } else {
+void propagate(int node, int start, int end) {
+    if (lazy_arr[node] != 0) {
+        seg[node] += (end - start + 1) * lazy_arr[node];
+        if (start != end) {
+            lazy_arr[2 * node] += lazy_arr[node];
+            lazy_arr[2 * node + 1] += lazy_arr[node];
+        }
+        lazy_arr[node] = 0;
+    }
+}
+
+void update_range(int node, int start, int end, int l, int r, ll val) {
+    propagate(node, start, end);
+    if (r < start || end < l)
+        return;
+    if (l <= start && end <= r) {
+        seg[node] += (end - start + 1) * val;
+        if (start != end) {
+            lazy_arr[2 * node] += val;
+            lazy_arr[2 * node + 1] += val;
+        }
+        return;
+    }
     int mid = (start + end) / 2;
-    build(2 * node, start, mid);
-    build(2 * node + 1, mid + 1, end);
-    tree[node] = tree[2 * node] + tree[2 * node + 1];
-  }
+    update_range(2 * node, start, mid, l, r, val);
+    update_range(2 * node + 1, mid + 1, end, l, r, val);
+    seg[node] = seg[2 * node] + seg[2 * node + 1];
 }
 
-void updateRange(int node, int start, int end, int l, int r, ll val) {
-  if (lazy[node] != 0) {
-    tree[node] += (end - start + 1) * lazy[node];
-    if (start != end) {
-      lazy[2 * node] += lazy[node];
-      lazy[2 * node + 1] += lazy[node];
-    }
-    lazy[node] = 0;
-  }
-  if (start > end || start > r || end < l)
-    return;
-  if (start >= l && end <= r) {
-    tree[node] += (end - start + 1) * val;
-    if (start != end) {
-      lazy[2 * node] += val;
-      lazy[2 * node + 1] += val;
-    }
-    return;
-  }
-  int mid = (start + end) / 2;
-  updateRange(2 * node, start, mid, l, r, val);
-  updateRange(2 * node + 1, mid + 1, end, l, r, val);
-  tree[node] = tree[2 * node] + tree[2 * node + 1];
-}
-
-ll queryRange(int node, int start, int end, int l, int r) {
-  if (lazy[node] != 0) {
-    tree[node] += (end - start + 1) * lazy[node];
-    if (start != end) {
-      lazy[2 * node] += lazy[node];
-      lazy[2 * node + 1] += lazy[node];
-    }
-    lazy[node] = 0;
-  }
-  if (start > end || start > r || end < l)
-    return 0;
-  if (start >= l && end <= r)
-    return tree[node];
-  int mid = (start + end) / 2;
-  ll left_sum = queryRange(2 * node, start, mid, l, r);
-  ll right_sum = queryRange(2 * node + 1, mid + 1, end, l, r);
-  return left_sum + right_sum;
+ll query_range(int node, int start, int end, int l, int r) {
+    propagate(node, start, end);
+    if (r < start || end < l)
+        return 0;
+    if (l <= start && end <= r)
+        return seg[node];
+    int mid = (start + end) / 2;
+    return query_range(2 * node, start, mid, l, r) + query_range(2 * node + 1, mid + 1, end, l, r);
 }
 
 void solve() {
-  cin >> n >> m >> k;
-  arr.resize(n + 1);
-  tree.resize(4 * n);
-  lazy.resize(4 * n);
-
-  for (int i = 1; i <= n; i++) {
-      cin >> arr[i];
-  }
-  build(1, 1, n);
-
-  for (int i = 0; i < m + k; i++) {
-      int a, b, c;
-      ll d;
-      cin >> a >> b >> c;
-      if (a == 1) {
-          cin >> d;
-          updateRange(1, 1, n, b, c, d);
-      } else if (a == 2) {
-          cout << queryRange(1, 1, n, b, c) << "\n";
-      }
-  }
+    int M, K;
+    cin >> N >> M >> K;
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+    build(1, 1, N);
+    for(int i = 0; i < M + K; i++) {
+        int type;
+        cin >> type;
+        if(type == 1){
+            int l, r;
+            ll val;
+            cin >> l >> r >> val;
+            update_range(1, 1, N, l, r, val);
+        }
+        else if(type == 2){
+            int l, r;
+            cin >> l >> r;
+            cout << query_range(1, 1, N, l, r) << "\n";
+        }
+    }
 }
 
 int main() {
-  // (void)freopen("input.txt", "r", stdin);
-
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-
-  solve();
-
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;   
 }
