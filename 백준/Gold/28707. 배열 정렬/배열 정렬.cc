973ec2e3d556a1e0b6536258899bbeac1dc04ad4
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2025-03-04 16:47:01 +0900

    [Gold I] Title: 배열 정렬, Time: 268 ms, Memory: 15388 KB -BaekjoonHub

---
+++ 백준/Gold/28707. 배열 정렬/배열 정렬.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll> 
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> A;
+set<vector<int>> visited;
+using T = pair<int, vector<int>>;
+priority_queue<T, vector<T>, greater<T>> pq;
+
+bool isSorted(vector<int>& v) {
+    for (int i = 0; i < v.size() - 1; i++) {
+        if (v[i] > v[i + 1]) return false;
+    }
+    return true;
+}
+
+void solve() {
+    cin >> N;
+    A.resize(N);
+    for (int& i : A) cin >> i;
+
+    cin >> M;
+    vector<tuple<int, int, int>> LRC(M);
+    for (auto& [l, r, c] : LRC)
+        cin >> l >> r >> c, --l, --r;
+
+    pq.push({ 0, A });
+
+    while (!pq.empty()) {
+        auto [d, v] = pq.top();
+        pq.pop();
+
+        if (visited.count(v))
+            continue;
+
+        visited.insert(v);
+        if (isSorted(v)) {
+            cout << d << '\n';
+            return;
+        }
+
+        for (auto [l, r, c] : LRC) {
+            auto T = v;
+            swap(T[l], T[r]);
+            pq.push({ d + c, T });
+        }
+    }
+
+    cout << -1 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int Tc = 1;
+    // cin >> Tc;
+    for (int tt = 1; tt <= Tc; tt++) {
+        solve();
+    }
+    return 0;
+}
