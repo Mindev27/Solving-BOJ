82a3978070e50395a9e8dc425167a399406a48f2
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2025-03-13 22:16:13 +0900

    [Silver IV] Title: 청기 백기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll> 
+
+ll N;
+
+void solve() {
+    cin >> N;
+
+    ll x = 1;
+    while ((x + 1) * (x + 1) <= N) x++;
+
+    cout << x << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int Tc = 1;
+    // cin >> Tc;
+    for (int tt = 1; tt <= Tc; tt++) {
+        solve();
+    }
+    return 0;
+}
