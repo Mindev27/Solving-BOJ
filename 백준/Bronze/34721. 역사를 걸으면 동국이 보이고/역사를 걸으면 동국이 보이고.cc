f188e7a3f9a709d0817c06f39ef23a5b3e18a2b7
LEE GANGMIN <126376329+Mindev27@users.noreply.github.com>
2025-11-09 23:39:47 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,35 @@
+/*
+writter : exzile_27
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define ld long double
+
+/*
+
+*/
+
+
+void solve(){
+    cout << "I love DGU\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int Tc = 1;
+    cin >> Tc;
+    for (int tt = 1; tt <= Tc; tt++) {
+        solve();
+    }
+ 
+    return 0;
+}
